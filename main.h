d957fa279367016b155244f07568bda49ffdfa86
vagrant <alungm1@gmail.com>
2022-11-22 17:19:38 +0000

    modify

--- main.h
+++ main.h
@@ -19,15 +19,14 @@
 #define S_SHORT 1
 
 /**
-* struct fmt = Struct op
-*
-@fmt: the format.
-@fn: the function associated.
+* struct fmt -> struct op
+*@fmt: the format.
+*@fn: the function associated.
 */
 struct fmt
 {
-    char fmt;
-    int (*fn) (va_list, char[], int, int, int, int);
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
 };
 
 /**
@@ -38,12 +37,15 @@ struct fmt
 **/
 typedef struct fmt fmt_t;
 
-int _printf(const char* format, ...);
-int handle_print(const char *fmt, int *i, va_list list, char buffer[], int flags, int width, int precision, int size );
+int _printf(const *char format, ...);
+int handle_print(const char *fmt, int *i, va_list list, char buffer[],
+int flags, int width, int precision, int size);
 
 /*****************FUNCTIONS**************************/
-int print_char(va_list types, char buffer[], int flags, int width, int prec, int size);
-int print_string(va_list types, char buffer[], int flags, int width, int prec, int  size );
+int print_char(va_list types, char buffer[], int flags,
+int width, int prec, int size);
+int print_string(va_list types, char buffer[], int flags,
+int width, int prec, int  size);
 int print_percent(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
